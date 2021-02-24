47da2013bb922038fc36c30e98e94874fd2059c3
ykamenashi <kamenashi@cloud-ace.jp>
2021-02-24 11:07:17 +0900

    1st

---
+++ index.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function _basher_err_quit() {
+    echo "$0: $1 quit." 1>&2
+    exit $2
+}
+
+if [ "$SHLVL" -ne 1 ] ; then ### is not login shell
+  if [ "$0" == 'index.bash' ] ; then
+    :
+  else
+    _basher_err_quit "not a login shell." 255
+  fi
+fi
+
+cd $(dirname 0) ### cd to This Directory
+
+if [ ! -d "lib" ] ; then
+  _basher_err_quit "'lib' directory not found. "
+fi
+
+exit 0
